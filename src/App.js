044e063860a4478ae835859e9ea762f0bf6244ec
amiinow101 <amiinow003@gmail.com>
2023-05-24 16:09:55 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import UpdateStates from './UpdateStates';
+import DisplayList from './DisplayList';
+import Buld from './Buld';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <UpdateStates />
+      <DisplayList />
+      <Buld />
     </div>
   );
 }
